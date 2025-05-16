421169b742a7b943f05f0912b8c8c4ab75320c9b
Danilo J. <danilojmini2@gmail.com>
2025-05-16 11:02:25 -0300

    Exercicio 7

---
+++ Ex07.js
@@ -0,0 +1,13 @@
+let opção = 2;
+
+if (opção <= 1) {
+    console.log("Opção 1: Cheeseburguer - R$15,00")
+}else if (opção <= 2){
+    console.log ("Opção 2: Batata Frita - R$10,00")
+}else if (opção <= 3){
+    console.log ("Opção 3: Milkshake - R$12,00")
+}else{ 
+    console.log("Produto inválido, fora do cardápio.")
+}
+
+//Exercicio correto
