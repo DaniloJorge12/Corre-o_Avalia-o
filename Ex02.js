4b052e859677c4821d42a5cbca229b4d799430e5
Danilo J. <danilojmini2@gmail.com>
2025-05-16 10:52:57 -0300

    Exercicio 1

---
+++ Ex02.js
@@ -0,0 +1,10 @@
+let tempoCorrida = 112;
+
+if (tempoCorrida < 90) 
+    console.log("Elite")
+else if (tempoCorrida >= 90 && tempoCorrida <= 150)
+    console.log ("Intermediário")
+else if (tempoCorrida >= 150)
+    console.log ("Iniciante")
+
+//Exercicio correto
