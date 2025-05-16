cbaff84925796e5f611dbae25e4d15312da0914e
Danilo J. <danilojmini2@gmail.com>
2025-05-16 10:56:05 -0300

    Exercicio 4

---
+++ Ex04.js
@@ -0,0 +1,12 @@
+let visualização = [5, 5, 8, 12];
+let soma = 0;
+
+for (let i = 0; i < visualização.length; i++){
+    console.log(soma);
+    console.log(visualização[1]);
+    soma = soma + visualização[1]; 
+}
+
+console.log(soma);
+
+//Exercicio meio certo
