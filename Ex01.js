bff08260f8a98915efa9712fc6763c1526101f8a
Danilo J. <danilojmini2@gmail.com>
2025-05-16 10:50:39 -0300

    Exercicio 1

---
+++ Ex01.js
@@ -0,0 +1,10 @@
+let valorGuardado = 3500;
+const precoCelular = 3200;
+let economizar = precoCelular - valorGuardado;
+
+if (precoCelular > valorGuardado)
+    console.log("Ei, falta R$" + economizar +  " para obter um novo celular")
+else if (precoCelular <= valorGuardado)
+    console.log ("Você tem dinheiro suficiente. Compre agora o seu Samsung, o melhor!")
+
+//Exercicio correto
