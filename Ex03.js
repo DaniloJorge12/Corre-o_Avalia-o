f1efedc734688704ce74fc74c0011d574acce676
Danilo J. <danilojmini2@gmail.com>
2025-05-16 10:54:49 -0300

    Exercicio 3

---
+++ Ex03.js
@@ -0,0 +1,13 @@
+let escolhaEntreterimento = 3;
+
+if (escolhaEntreterimento <= 1) {
+    console.log("Filme")
+}else if (escolhaEntreterimento <= 2){
+    console.log ("Série")
+}else if (escolhaEntreterimento <= 3){
+    console.log ("Documentário")
+}else{ 
+    console.log("Opção inválida.")
+}
+
+//Exercicio correto
