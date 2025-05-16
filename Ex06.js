a994319c624854bfda260637bac1f37b32ac1163
Danilo J. <danilojmini2@gmail.com>
2025-05-16 11:01:11 -0300

    Exercicio 6

---
+++ Ex06.js
@@ -0,0 +1,11 @@
+let passeios = [1 + 2 + 3 + 2 + 1];
+
+console.log("Foram feitos " + passeios + " passeios.");
+
+if (passeios > 8){
+    console.log("Ciclista profissional!")
+}else{
+    console.log("Continue pedalando!")
+}
+
+//Exercicio correto
